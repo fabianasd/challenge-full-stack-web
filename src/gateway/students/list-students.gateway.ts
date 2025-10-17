f26ba5382d15bad95aef3c74ef073bdef52691dc
Fabiana Petrovick <fafadiaz@hotmail.com>
2025-10-17 08:43:04 -0300

    docs: geração da documentação da listagem e registro de alunos

--- src/gateway/students/list-students.gateway.ts
+++ src/gateway/students/list-students.gateway.ts
@@ -3,12 +3,10 @@ import { type PersonWithStudent, UsersRepository } from "../../repositories/user
 import { CommonGateway } from "../common/common.gateway";
 
 export class ListStudentsGateway extends CommonGateway {
-  constructor(private usersRepository: UsersRepository) {
-      super();
-  }
-
-
-  
+    constructor(private usersRepository: UsersRepository) {
+        super();
+    }
+    
     async listAllStudents(): Promise<StudentEntity[]> {
         const users = await this.usersRepository.listAll();
         const students: StudentEntity[] = users
--- src/shared/errors/students.error.ts
+++ src/shared/errors/students.error.ts
@@ -9,15 +9,3 @@ export class StudentError extends Error {
         this.statusCode = statusCode
     }
 }
-
-export class CpfAlreadyInUseError extends StudentError {
-    constructor() {
-        super(ERROR_MESSAGES.DOCUMENT_ALREADY_IN_USE, ERROR_TYPE.CPF_ALREADY_IN_USE, HTTP_STATUS.CONFLICT)
-    }
-}
-
-export class UserAlreadyExistsError extends StudentError {
-    constructor() {
-        super(ERROR_MESSAGES.EMAIL_ALREADY_IN_USE, ERROR_TYPE.STUDENT_ALREADY_REGISTERED, HTTP_STATUS.CONFLICT)
-    }
-}
